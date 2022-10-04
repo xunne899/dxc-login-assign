7617a2b666c96cd258dbdbf48522694615fa449d
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-10-04 18:50:43 +0000

    add heroku

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,12 +1,17 @@
-// Setting up the database connection
+
 const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      user: 'foo',
-      password:'bar',
-      database:'tech'
+  client: process.env.DB_DRIVER,
+  connection: {
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    host:process.env.DB_HOST,
+    ssl: {
+        'rejectUnauthorized': false
     }
-  })
-  const bookshelf = require('bookshelf')(knex)
-  
-  module.exports = bookshelf;
+
+  }
+})
+const bookshelf = require('bookshelf')(knex)
+
+module.exports = bookshelf;
